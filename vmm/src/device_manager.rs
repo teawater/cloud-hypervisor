869013347e49692ea96fe582b651323a27db11b6
Hui Zhu <teawater@antfin.com>
2020-03-04 10:16:27 +0800

    vmm: device: Add make_virtio_mem_devices
    Add make_virtio_mem_devices to add virtio-mem to vmm.
    
    Signed-off-by: Hui Zhu <teawater@antfin.com>

--- vmm/src/device_manager.rs
+++ vmm/src/device_manager.rs
@@ -258,6 +258,15 @@ pub enum DeviceManagerError {
 
     // No disk path was specified when one was expected
     NoDiskPath,
+
+    /// Cannot create virtio-mem device
+    CreateVirtioMem(io::Error),
+
+    /// Cannot try Clone virtio-mem resize
+    TryCloneVirtioMemResize(vm_virtio::mem::Error),
+
+    /// Cannot find a memory range for virtio-mem memory
+    VirtioMemRangeAllocation,
 }
 pub type DeviceManagerResult<T> = result::Result<T, DeviceManagerError>;
 
@@ -1017,6 +1026,8 @@ impl DeviceManager {
         // Add virtio-vsock if required
         devices.append(&mut self.make_virtio_vsock_devices()?);
 
+        devices.append(&mut self.make_virtio_mem_devices()?);
+
         Ok(devices)
     }
 
@@ -1479,6 +1490,33 @@ impl DeviceManager {
                 self.migratable_devices
                     .push(Arc::clone(&vsock_device) as Arc<Mutex<dyn Migratable>>);
             }
+        }
+
+        Ok(devices)
+    }
+
+    fn make_virtio_mem_devices(&mut self) -> DeviceManagerResult<Vec<(VirtioDeviceArc, bool)>> {
+        let mut devices = Vec::new();
+
+        let mm = &self.memory_manager.lock().unwrap();
+        if let (Some(region), Some(resize)) = (&mm.virtiomem_region, &mm.virtiomem_resize) {
+            let virtio_mem_device = Arc::new(Mutex::new(
+                vm_virtio::Mem::new(
+                    &region,
+                    resize
+                        .try_clone()
+                        .map_err(DeviceManagerError::TryCloneVirtioMemResize)?,
+                )
+                .map_err(DeviceManagerError::CreateVirtioMem)?,
+            ));
+
+            devices.push((
+                Arc::clone(&virtio_mem_device) as Arc<Mutex<dyn vm_virtio::VirtioDevice>>,
+                false,
+            ));
+
+            self.migratable_devices
+                .push(Arc::clone(&virtio_mem_device) as Arc<Mutex<dyn Migratable>>);
         }
 
         Ok(devices)
